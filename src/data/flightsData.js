962c28bb648a8d44740c113ac38fe0ce40bd2712
shafiq03 <mohamedshafiq2281@gmail.com>
2026-05-07 20:51:28 +0530

    files

---
+++ src/data/flightsData.js
@@ -0,0 +1,15 @@
+const flights = [
+  {
+    id: 1,
+    airline: "Emirates",
+    from: "Chennai",
+    to: "Dubai",
+    departure: "10:00 AM",
+    arrival: "1:00 PM",
+    price: 25000,
+    seats: 25,
+    image: "cloudinary-image-url"
+  }
+];
+
+export default flights;
